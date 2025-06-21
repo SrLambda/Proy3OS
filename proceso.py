5b668eda41b5b9148414f5695b821df1f389d2ad
JeRee69 <jiturriaga21@alumnos.utalca.cl>
2025-06-20 22:39:35 -0400

    Se agrega tabla y ademas un boton de agregar proceso(Aun no funka)

--- proceso.py
+++ proceso.py
@@ -2,8 +2,9 @@ import time
 import random
 
 class Proceso:
-    def __init__(self, pid, tiempo_llegada, duracion, tamano_memoria):
+    def __init__(self, pid,nombre="", tiempo_llegada=0, duracion=0, tamano_memoria=0,):
         self.pid = pid  # ID del proceso
+        self.nombre = nombre  # Asegúrate de tener este atributo
         self.tiempo_llegada = tiempo_llegada  # Momento en que el proceso llega al sistema 
         self.duracion = duracion  # Tiempo total de CPU que necesita el proceso (CPU burst) 
         self.tiempo_restante = duracion # Tiempo restante de ejecución
@@ -19,9 +20,11 @@ class Proceso:
     def __repr__(self):
         return f"P{self.pid} (Estado: {self.estado}, Dur: {self.duracion}, Rest: {self.tiempo_restante}, Mem: {self.tamano_memoria})"
 
-    def actualizar_tiempo_restante(self, tiempo):
-        self.tiempo_restante -= tiempo
-        self.tiempo_en_cpu += tiempo
+    def actualizar_tiempo_restante(self, tiempo_unidad):
+        """Actualiza el tiempo restante del proceso"""
+        self.tiempo_restante = max(0, self.tiempo_restante - tiempo_unidad)
+        if self.tiempo_restante == 0:
+            self.set_estado("terminado")
 
     def set_estado(self, nuevo_estado):
         self.estado = nuevo_estado
